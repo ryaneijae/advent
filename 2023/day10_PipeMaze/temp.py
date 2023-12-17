f6097d117a6406173fe737bea8e177eca705e659
Ryan Cho <ryan@pattern.bio>
2023-12-17 14:57:35 -0600

    Adding prompt.txt for past few days

--- 2023/day10_PipeMaze/temp.py
+++
@@ -1,9 +0,0 @@
-def intersection(l, k):
-    j = [value for value in l if value in k]
-    return j
-
-a = [(0, 2), (2, 2)]
-b = [(1, 1), (2, 1), (3, 1), (3, 2), (3, 3), (2, 3), (1, 3), (1, 2)]
-
-if intersection(a, b):
-    print('Hi')
